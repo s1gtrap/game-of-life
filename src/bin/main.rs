c8824f9eb8502e2a16283a04bd1f017973626f57
William Tange <will@brt.sexy>
2022-04-18 21:56:21 +0200

    Add more examples

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,20 +1,67 @@
 #![feature(stdin_forwarders)]
 
+const BLINKER: (usize, usize, &'static str) = (
+    3,
+    3,
+    r#"     
+  .  
+  .  
+  .  
+     "#,
+);
+const TOAD: (usize, usize, &'static str) = (
+    4,
+    4,
+    r#"      
+   .  
+ .  . 
+ .  . 
+  .   
+      
+   "#,
+);
+const BEACON: (usize, usize, &'static str) = (
+    4,
+    4,
+    r#"      
+ ..   
+ .    
+    . 
+   .. 
+      
+   "#,
+);
+const PULSAR: (usize, usize, &'static str) = (
+    15,
+    15,
+    r#"                 
+                 
+    ...   ...    
+                 
+  .    . .    .  
+  .    . .    .  
+  .    . .    .  
+    ...   ...    
+                 
+    ...   ...    
+  .    . .    .  
+  .    . .    .  
+  .    . .    .  
+                 
+    ...   ...    
+                 
+                 "#,
+);
+
 fn main() {
-    let w = 3;
-    let h = 3;
+    let (w, h, shape) = PULSAR;
     let mut counts = vec![0; (w + 2) * (h + 2)];
-    let mut states: Vec<_> = r#"     
-  .  
-  .  
-  .  
-     
-"#
-    .lines()
-    .map(|l| l.chars())
-    .flatten()
-    .map(|c| c == '.')
-    .collect();
+    let mut states: Vec<_> = shape
+        .lines()
+        .map(|l| l.chars())
+        .flatten()
+        .map(|c| c == '.')
+        .collect();
     let print = |states: &[bool]| {
         println!("+{:-<1$}+", "", w + 2);
         for i in 0..(h + 2) {
